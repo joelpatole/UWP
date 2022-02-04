90a57b9b91bbe4a639ed41ea8c14c269c4256399
HarpreetKaurSohal <preet3089996@gmail.com>
2022-02-04 19:43:49 +0530

    4th feb 2022,friday

---
+++ Pizza.com/Pizza.com/Model/Pizza.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Pizza.com.Model
+{
+    public class Pizza1 : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public String Pname;
+        public int Pcost;
+        public String Pdesc;
+
+        public String PizzaName
+        {
+            get => Pname;
+            set
+            {
+                Pname = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(PizzaName)));           
+            }
+        }
+
+        public int PizzaPrice
+        {
+            get => Pcost;
+            set
+            {
+                Pcost = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(PizzaPrice)));
+            }
+        }
+
+        public String PizzaDescription
+        {
+            get => Pdesc;
+            set
+            {
+                Pdesc = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(PizzaDescription)));
+            }
+        }
+
+    }
+}
--- Pizza.com/Pizza.com/VegMenu.xaml.cs
+++ Pizza.com/Pizza.com/VegMenu.xaml.cs
@@ -12,6 +12,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Pizza.com.Model;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -22,6 +23,7 @@ namespace Pizza.com
     /// </summary>
     public sealed partial class VegMenu : Page
     {
+        List<Pizza1> selectedItem = new List<Pizza1>();
         public VegMenu()
         {
             this.InitializeComponent();
@@ -42,5 +44,34 @@ namespace Pizza.com
         {
              this.Frame.Navigate(typeof(MainPage));
         }
+
+        private void CheckBox_IsCheckedChanged(object sender, RoutedEventArgs e)
+        {
+            //UpdateCart();
+        }
+
+        private void AddButtonVegClick(object sender, RoutedEventArgs e)
+        {
+            //Cart cart = new Cart(selectedItem);
+            
+            this.Frame.Navigate(typeof(Cart),selectedItem);
+
+        }
+
+        private void VegMenuList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            foreach (var item in e.AddedItems.OfType<Model.Pizza1>()) 
+            {
+                selectedItem.Add(item);       
+            }
+        }
+        /* private void UpdateCart()
+{ 
+Cart cart=cartListview.SeletedItem as Cart;
+if(cart!=null)
+{
+
+}
+}*/
     }
 }
